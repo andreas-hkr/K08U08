5ee0a8182c7e0e58262c53c01075d1de03dcc7fb
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-16 18:20:02 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Skriv en metod som tar en text som parameter och returnerar
+        // texten med alla mellanslag borttagna.
+
+
+    }
+}
